dae2564940ac97ec96902d3f6c660bf636e27272
Sellyna <sellynalky@gmail.com>
2022-08-16 21:47:34 +1200

    update commit

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,52 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import Logo from "./partials/Logo";
+import FooterNav from "./partials/FooterNav";
+import FooterSocial from "./partials/FooterSocial";
+
+const propTypes = {
+  topOuterDivider: PropTypes.bool,
+  topDivider: PropTypes.bool,
+};
+
+const defaultProps = {
+  topOuterDivider: false,
+  topDivider: false,
+};
+
+const Footer = ({ className, topOuterDivider, topDivider, ...props }) => {
+  const classes = classNames(
+    "site-footer center-content-mobile",
+    topOuterDivider && "has-top-divider",
+    className
+  );
+
+  return (
+    <footer {...props} className={classes}>
+      <div className="container reveal-from-top" data-reveal-delay="800">
+        <div
+          className={classNames(
+            "site-footer-inner",
+            topDivider && "has-top-divider"
+          )}
+        >
+          <div className="footer-top space-between text-xxs">
+            <Logo />
+          </div>
+          <div className="footer-bottom space-between text-xxs invert-order-desktop">
+            <div className="footer-copyright">
+              Made by <a href="#">The Dream Team</a>. All right reserved to the
+              5 members.
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+Footer.propTypes = propTypes;
+Footer.defaultProps = defaultProps;
+
+export default Footer;
---
+++ src/components/sections/Hero.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import classNames from "classnames";
+import { SectionProps } from "../../utils/SectionProps";
+import ButtonGroup from "../elements/ButtonGroup";
+import Button from "../elements/Button";
+import Image from "../elements/Image";
+import Modal from "../elements/Modal";
+
+const propTypes = {
+  ...SectionProps.types,
+};
+
+const defaultProps = {
+  ...SectionProps.defaults,
+};
+
+const Hero = ({
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  ...props
+}) => {
+  const outerClasses = classNames(
+    "hero section center-content",
+    topOuterDivider && "has-top-divider",
+    bottomOuterDivider && "has-bottom-divider",
+    hasBgColor && "has-bg-color",
+    invertColor && "invert-color",
+    className
+  );
+
+  const innerClasses = classNames(
+    "hero-inner section-inner",
+    topDivider && "has-top-divider",
+    bottomDivider && "has-bottom-divider"
+  );
+
+  return (
+    <section {...props} className={outerClasses}>
+      <div className="container-sm">
+        <div className={innerClasses}>
+          <div className="hero-content">
+            <h1
+              className="mt-0 mb-16 reveal-from-bottom"
+              data-reveal-delay="200"
+            >
+              Find the car you have been{" "}
+              <span className="text-color-primary">looking</span> for.
+            </h1>
+            <div className="container-xs">
+              <p
+                className="m-0 mb-32 reveal-from-bottom"
+                data-reveal-delay="400"
+              >
+                See what AWS Rekognition can do. Upload a picture using the
+                button below...
+              </p>
+              <div className="reveal-from-bottom" data-reveal-delay="600">
+                <ButtonGroup>
+                  <form>
+                    <input type="file" name="image" />
+                    <Button color="primary" type="submit">
+                      Upload
+                    </Button>
+                  </form>
+                </ButtonGroup>
+              </div>
+            </div>
+          </div>
+          <div
+            className="container-xs reveal-from-bottom container_hello"
+            data-reveal-delay="700"
+          >
+            <h1>Hello World!</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+Hero.propTypes = propTypes;
+Hero.defaultProps = defaultProps;
+
+export default Hero;
