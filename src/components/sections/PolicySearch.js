1e5d0f194b85d5ddbfad488e01d5a667367079b7
Sellyna <sellynalky@gmail.com>
2022-08-26 15:15:09 +1200

    ray pls help

--- src/components/sections/PolicySearch.js
+++ src/components/sections/PolicySearch.js
@@ -1,6 +1,7 @@
 import React from "react";
 import classNames from "classnames";
 import Input from "../elements/Input";
+import Button from "../elements/Button";
 
 const PolicySearch = ({
   className,
@@ -38,24 +39,11 @@ const PolicySearch = ({
         </div>
         <Input
           id="query"
-          type="search"
-          label="Subscribe"
-          labelHidden
+          type="text"
           hasIcon="right"
-          placeholder="Your best email"
-        >
-          <svg
-            width="16"
-            height="12"
-            xmlns="http://www.w3.org/2000/svg"
-            type="submit"
-          >
-            <path
-              d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z"
-              fill="#376DF9"
-            />
-          </svg>
-        </Input>
+          placeholder="Search our policies"
+        ></Input>
+        <Button color="primary"></Button>
       </div>
     </section>
   );
